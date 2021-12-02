e0c8bf692d7b8a8a25551597363750c351cdf61d
Kirychenka Yauheni <geka10058@mail.ru>
2021-12-02 11:56:27 +0300

    add: nav_graph (works)

--- app/src/main/java/com/example/todo/ui/MainActivity.kt
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.todo
+package com.example.todo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/todo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.example.todo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.todo.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+}
