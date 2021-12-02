1c312787b9df86bba12172e82b6fff7ffb4c2058
Kirychenka Yauheni <geka10058@mail.ru>
2021-12-03 01:09:20 +0300

    add: capacity to delete and undo deleting tasks (works per swipes)

--- app/src/main/java/com/example/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/todo/ui/tasks/TasksViewModel.kt
@@ -8,9 +8,11 @@ import com.example.todo.data.PreferencesManager
 import com.example.todo.data.SortOrder
 import com.example.todo.data.Task
 import com.example.todo.data.TaskDao
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
@@ -24,6 +26,9 @@ class TasksViewModel @ViewModelInject constructor(
     /*val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
     val hideCompleted = MutableStateFlow(false)
 */
+    private val tasksEventChannel = Channel<TasksEvent>()
+    val tasksEvent = tasksEventChannel.receiveAsFlow()
+
     private val tasksFlow = combine(
         searchQuery,
         preferencesFlow
@@ -43,11 +48,22 @@ class TasksViewModel @ViewModelInject constructor(
         preferencesManager.updateHideCompleted(hideCompleted)
     }
 
-    fun onTaskSelected(task:Task){
-
-    }
+    fun onTaskSelected(task:Task){}
 
     fun onTaskCheckedChanged(task: Task, isChecked: Boolean) = viewModelScope.launch {
         taskDao.update(task.copy(completed = isChecked))
+    }
+
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.delete(task)
+        tasksEventChannel.send(TasksEvent.ShowUndoDeleteTaskMessage(task))
+    }
+
+    fun onUndoDeleteClick(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+    }
+
+    sealed class TasksEvent{
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TasksEvent()
     }
 }
